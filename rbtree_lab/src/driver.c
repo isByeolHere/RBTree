feb5c5f0e8e57ad3e6b648889c344e341ca7095d
Bell-isHere <quf417@gmail.com>
2025-04-22 10:47:57 +0900

    삽입 구현

--- rbtree_lab/src/driver.c
+++ rbtree_lab/src/driver.c
@@ -1,5 +1,95 @@
 #include "rbtree.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
+    int c, i;
+    c = 1;
+    // 레드블랙 트리 시작
+    rbtree *tree = new_rbtree();
+    printf("1: 레드블랙 트리에 정수 삽입\n");
+    printf("2: 레드블랙 트리에서 정수 검색\n");
+    printf("3: 레드블랙 트리에서 정수 삭제\n");
+    printf("4: 레드블랙 트리에서 최소값 검색\n");
+    printf("5: 레드블랙 트리에서 최대값 검색\n");
+    printf("6: 레드블랙 트리에서 정수 배열로 변환\n");
+    printf("7: 레드블랙 트리 삭제\n");
+    printf("0: 종료\n");
+    while (c != 0)
+    {
+        printf("명령어를 입력하세요: ");
+        scanf("%d", &c);
+        switch (c)
+        {
+        case 1:
+            printf("삽입할 정수를 입력하세요: ");
+            scanf("%d", &i);
+            rbtree_insert(tree, i);
+            break;
+        case 2:
+            printf("검색할 정수를 입력하세요: ");
+            scanf("%d", &i);
+            node_t *node = rbtree_find(tree, i);
+            if (node != NULL)
+                printf("정수 %d가 트리에 존재합니다.\n", i);
+            else
+                printf("정수 %d가 트리에 존재하지 않습니다.\n", i);
+            break;
+        case 3:
+            printf("삭제할 정수를 입력하세요: ");
+            scanf("%d", &i);
+            node_t *node_to_delete = rbtree_find(tree, i);
+            if (node_to_delete != NULL) {
+                rbtree_erase(tree, node_to_delete);
+                printf("정수 %d가 삭제되었습니다.\n", i);
+            } else {
+                printf("정수 %d가 트리에 존재하지 않습니다.\n", i);
+            }
+            break;
+        case 4:
+            node_t *min_node = rbtree_min(tree);
+            if (min_node != NULL)
+                printf("최소값은 %d입니다.\n", min_node->key);
+            else
+                printf("트리가 비어 있습니다.\n");
+            break;
+        case 5:
+            node_t *max_node = rbtree_max(tree);
+            if (max_node != NULL)
+                printf("최대값은 %d입니다.\n", max_node->key);
+            else
+                printf("트리가 비어 있습니다.\n");
+            break;
+        case 6:
+            // 배열 크기 설정
+            size_t n = 100; // 예시로 100으로 설정
+            key_t *arr = (key_t *)malloc(n * sizeof(key_t));
+            int count = rbtree_to_array(tree, arr, n);
+            if (count == 0)
+                printf("트리가 비어 있습니다.\n");
+            else
+            {
+                printf("트리의 정수 배열: ");
+                for (int j = 0; j < count; j++)
+                {
+                    printf("%d ", arr[j]);
+                }
+                printf("\n");
+            }
+            free(arr); // 동적 메모리 해제
+            break;
+        case 7:
+            delete_rbtree(tree);
+            tree = new_rbtree(); // 새로운 트리 생성
+            printf("레드블랙 트리가 삭제되었습니다.\n");
+            break;
+        case 0:
+            printf("종료합니다.\n");
+            break;
+        default:
+            printf("잘못된 명령어입니다.\n");
+            break;
+        }
+    }
 }
--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -2,10 +2,20 @@
 
 #include <stdlib.h>
 
+void rb_insert_fixup(rbtree *t, node_t *z);
+void rotate_left(rbtree *t, node_t *x);
+void rotate_right(rbtree *t, node_t *x);
+
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  rbtree *t = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
-  return p;
+  node_t *nil_node = (node_t *)calloc(1, sizeof(node_t));
+
+  nil_node->color = RBTREE_BLACK;
+  nil_node->left = nil_node->right = nil_node->parent = nil_node;
+  t->nil =  t->root = nil_node;
+  
+  return t;
 }
 
 void delete_rbtree(rbtree *t) {
@@ -14,8 +24,123 @@ void delete_rbtree(rbtree *t) {
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
+  node_t *z = (node_t*)malloc(sizeof(node_t));
+  if (z == NULL) return NULL;
+  
+  z->key = key;
+  z->color = RBTREE_RED;
+  z->parent = z->left = z->right = t->nil;
+
+  node_t *x = t->root;
+  node_t *y = t->nil;
+
+  while (x != t->nil){
+    y = x;
+    if (z->key < x->key){
+      x = x->left;
+    } else {x = x->left;}
+    
+    z->parent = y;
+    if (y == t->nil){
+      t->root = z;
+    } else if (z->key<y->key){
+      y->left = z;
+    } else {
+      y->right = z;
+    }
+    
+    z->left = z->right = t->nil;
+    z->color = RBTREE_RED;
+    rb_insert_fixup(t, z);
+  }
   return t->root;
+}
+
+// 삽입/삭제 fixup
+void rb_insert_fixup(rbtree *t, node_t *z){
+  node_t *uncle;
+
+  while (z->parent->color == RBTREE_RED){
+    if (z->parent == z->parent->parent->left){
+      uncle = z->parent->parent->right;
+      if (uncle->color == RBTREE_RED){
+        z->parent->color = RBTREE_BLACK;
+        uncle->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->right){
+          z = z->parent;
+          rotate_left(t, z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent = RBTREE_RED;
+        rotate_right(t, z->parent->parent);
+      }
+    } else {
+      uncle = z->parent->parent->left;
+      if (uncle->color == RBTREE_RED){
+        z->parent->color = RBTREE_BLACK;
+        uncle->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->left){
+          z = z->parent;
+          rotate_right(t,z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        rotate_left(t, z->parent->parent);
+      }
+    }
+  }
+  t->root->color = RBTREE_BLACK;
+};
+
+
+void rotate_left(rbtree *t, node_t *x){
+  node_t *y = x->right;
+  x->right = y->left;
+  if (y->left != t->nil){
+    y->left->parent = x;
+  }
+  y->parent = x->parent;
+  if (x->parent == t->nil){
+    t->root = y;
+  } else if (x == x->parent->left)
+  {
+    x->parent->left = y;
+  } else {
+    x->parent->right = y;
+  }
+  y->left = x;
+  x->parent = y;
+}
+
+void rotate_right(rbtree *t, node_t *x){
+  node_t *y = x->left;
+  x->left = y->right;
+  if (y->right != t->nil){
+    y->right->parent = x;
+  };
+  y->parent = x->parent;
+  if (x->parent == t->nil){
+    t->root = y;
+  } else if (x == x->parent->right)
+  {
+    x->parent->right = y;
+  } else {
+    x->parent->left = y;
+  }
+  y->right = x;
+  x->parent = y;
+}
+
+
+
+void rb_erase_fixup(rbtree *t, node_t *x){
+  
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
